c48c87ffd228c72c93232e84094e2decd746e3b5
eric_G <46165813+ericgaspar@users.noreply.github.com>
2025-07-12 19:48:51 +0200

    Update server.js

--- conf/server.js
+++ conf/server.js
@@ -1,23 +1,17 @@
 const path = require('path');
-const Armadietto = require('__MODULE_PATH__/armadietto');
+const Armadietto = require('../lib/armadietto');
 let store;
-let server;
 
-// const type = process.argv[2];
+const type = process.argv[2];
 
-store = new Armadietto.FileTree({path: '__DATA_DIR__/storage'});
+store = new Armadietto.FileTree({ path: '__DATA_DIR__/storage' });
 
-server = new Armadietto({
-  store: store,
+const server = new Armadietto({
+  store,
   http: {
+    host: '127.0.0.1',
     port: __PORT__
   },
-  // https: {
-  //   force: true,
-  //   port: __PORT__,
-  //   cert: '/etc/yunohost/certs/__DOMAIN__/crt.pem',
-  //   key: '/etc/yunohost/certs/__DOMAIN__/key.pem'
-  // },
   allow: {
     signup: __IS_SIGNUP__
   },
